09fa25400fabdf95b36105ae2fa55f0427d20424
Daniela Brailovsky <dbrailo@gmail.com>
2021-07-19 19:52:35 +0300

    Added Exercise 01

---
+++ Week_03/Day_02/Ex. XP/script.js
@@ -0,0 +1,17 @@
+// Guess the correct outcome to the lines of code written below.
+// After guessing the outcome check the results of the code below.
+// Check them by copying line by line and running them in the console.
+
+5 + "34" //534
+5 - "4" // 1
+10 % 5 // 0
+5 % 10 // 5
+"Java" + "Script" // JavaScript"
+" " + " " // "  "
+" " + 0 // " 0"
+true + true // 2
+true + false // 1
+false + true // 1
+false - true // -1
+3 - 4 // -1
+"Bob" - "bill" // NaN
